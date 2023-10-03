c5316831731b1052dc7b5e80ffc5f27343294e5f
armitage <scott@nihilanth.co.uk>
2023-10-03 07:57:28 +0100

    Better menu (work in progress)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import Menu from './Menu'; // Import the Menu component
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // Add state for menu open/close
+
+  // Function to toggle menu open/close
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <div className="burger-menu">
-        <button className="burger-button">&#9776;</button>
+        <button className="burger-button" onClick={toggleMenu}>&#9776;</button> {/* Add onClick listener */}
       </div>
       <h1 className="header-title">eLearn</h1>
       <div className="header-buttons">
         <button className="action-button">Log in</button>
         <button className="action-button">Sign Up</button>
       </div>
+      {menuOpen && <Menu />} {/* Render the Menu component when menuOpen is true */}
     </header>
   );
 };
---
+++ src/components/Menu.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./Menu.css"; // Import the CSS file for styling
+
+const Menu = () => {
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  const handleCategoryHover = (category) => {
+    setActiveCategory(category);
+  };
+
+  return (
+    <div className="menu-container">
+      <ul className="menu">
+        <li
+          className={`menu-item ${activeCategory === "Programming" ? "active" : ""}`}
+          onMouseEnter={() => handleCategoryHover("Programming")}
+        >
+          Programming
+          {activeCategory === "Programming" && (
+            <ul className="sub-menu">
+              <li>Web Development</li>
+              <li>Mobile App Development</li>
+              <li>Data Science</li>
+              <li>Game Development</li>
+            </ul>
+          )}
+        </li>
+        <li
+          className={`menu-item ${activeCategory === "Technology" ? "active" : ""}`}
+          onMouseEnter={() => handleCategoryHover("Technology")}
+        >
+          Technology
+          {activeCategory === "Technology" && (
+            <ul className="sub-menu">
+              <li>Cybersecurity</li>
+              <li>Cloud Computing</li>
+              <li>Internet of Things (IoT)</li>
+              <li>Artificial Intelligence (AI)</li>
+            </ul>
+          )}
+        </li>
+        <li
+          className={`menu-item ${activeCategory === "Engineering" ? "active" : ""}`}
+          onMouseEnter={() => handleCategoryHover("Engineering")}
+        >
+          Engineering
+          {activeCategory === "Engineering" && (
+            <ul className="sub-menu">
+              <li>Electrical Engineering</li>
+              <li>Mechanical Engineering</li>
+              <li>Civil Engineering</li>
+              <li>Chemical Engineering</li>
+            </ul>
+          )}
+        </li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
