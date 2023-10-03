a86d406115320107fcfc03ce19af29dbe5756bc3
armitage <scott@nihilanth.co.uk>
2023-10-03 22:02:01 +0100

    layout fix and about page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Layout from "./components/Layout";
-import HomePage from "./pages/HomePage";
-
-import './App.css';
 
 const App = () => {
-
   return (
-    <Layout>
-      <HomePage/>
-    </Layout>
+    <Router>
+      <Layout> {/* Wrap the Routes component with Layout */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 };
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import './Layout.css';
 
 function Layout({ children }) {
   return (
     <div>
       <Header />
-      {/* Main content */}
-      {children}
+      <div className="main-content" style={{ marginTop: "70px" }}>
+        {/* Use a div with class name */}
+        {children}
+      </div>
       <Footer />
     </div>
   );
