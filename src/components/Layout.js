bc21c4604340d4f755ca7e248e8a107c27a134ce
armitage <scott@nihilanth.co.uk>
2023-10-04 07:01:28 +0100

    footer css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import './Layout.css';
+import './Layout.css'; // Make sure the path to Layout.css is correct
 
 function Layout({ children }) {
   return (
