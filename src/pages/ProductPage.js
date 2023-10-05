58313e8fa9e6532b44233767df163e9a58b75a90
armitage <scott@nihilanth.co.uk>
2023-10-05 06:53:03 +0100

    contents overhaul

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
+const CourseContent = ({ content }) => {
+  // Parse the JSON object
+  const parsedContent = JSON.parse(content);
+
+  // Access the course_contents array
+  const courseContentList = parsedContent.course_contents.map((item, index) => (
+    <li key={index}>{item}</li>
+  ));
+
+  return (
+    <div>
+      <ul>
+        {courseContentList}
+      </ul>
+    </div>
+  );
+};
+
 const ProductPage = ({ course }) => {
   return (
     <div>
       <h1>{course.title}</h1>
       <p>{course.description}</p>
+      <h2>What you'll learn</h2>
+      <CourseContent content={course.content} />
     </div>
   );
 };
