d8fc992751996fc9306401e4dd4b811ec6d69852
armitage <scott@nihilanth.co.uk>
2023-10-05 22:41:42 +0100

    link header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Header.css';
 import Menu from './Menu'; // Import the Menu component
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false); // Add state for menu open/close
@@ -15,7 +16,9 @@ const Header = () => {
       <div className="burger-menu">
         <button className="burger-button" onClick={toggleMenu}>&#9776;</button> {/* Add onClick listener */}
       </div>
-      <h1 className="header-title">eLearn</h1>
+      <Link to="/">
+        <h1 className="header-title">eLearn</h1>
+      </Link>
       <div className="header-buttons">
         <button className="action-button">Log in</button>
         <button className="action-button">Sign Up</button>
