23b4af2ad9a597a3ca54723db9169e51666f997f
armitage <scott@nihilanth.co.uk>
2023-10-08 20:57:00 +0100

    api fix

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ const BASE_URL = "http://127.0.0.1:5002";
 
 export const getRecords = async (endpoint) => {
   try {
-    const response = await fetch(`${BASE_URL}/api/collections/${endpoint}/records`);
+    const response = await fetch(`${BASE_URL}/api/collections/${endpoint}/records?perPage=100`);
     const data = await response.json();
     return data.items;
   } catch (error) {
