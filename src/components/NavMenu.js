e70e2018a2922224cb8727d5c8319343bf1a457e
armitage <scott@nihilanth.co.uk>
2023-10-22 23:09:43 +0100

    hide menu when closed

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import './NavMenu.css';
 
 function NavMenu() {
+  const [showNav, setShowNav] = useState(false);
   const [showCategories, setShowCategories] = useState(false);
   const [showSubmenu, setShowSubmenu] = useState({
     programming: false,
     technology: false,
     engineering: false
   });
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
 
   const toggleCategories = () => {
     setShowCategories(!showCategories);
@@ -26,6 +31,7 @@ function NavMenu() {
   };
 
   const resetMenu = () => {
+    setShowNav(false);
     setShowCategories(false);
     setShowSubmenu({
       programming: false,
@@ -35,97 +41,110 @@ function NavMenu() {
   };
 
   return (
-    <div className="primary-nav" onMouseLeave={resetMenu}>
-      <nav role="navigation" className="menu">
-        <li>
-          <a href="#">Dashboard</a>
-          <span className="icon">
-            <i className="fa fa-dashboard"></i>
-          </span>
-        </li>
-        <a href="#" className="logotype" onClick={toggleCategories}>
-          Categories
-          <span className={`downarrow ${showCategories ? 'open' : ''}`}>
-            <i className="fa fa-caret-down"></i>
-          </span>
-        </a>
+    <div
+      className={`primary-nav ${showNav ? 'show' : ''}`}
+      onMouseEnter={toggleNav}
+      onMouseLeave={resetMenu}
+    >
+      {showNav && (
+        <nav role="navigation" className="menu">
+          {/* Dashboard */}
+          <li aria-hidden={!showCategories}>
+            <a href="#">Dashboard</a>
+            <span className="icon">
+              <i className="fa fa-dashboard"></i>
+            </span>
+          </li>
 
-        {/* Programming submenu */}
-        {showCategories && (
-          <ul className="menu-dropdown">
-            <li>
-              <a href="#" onClick={() => toggleSubmenu('programming')}>
-                Programming
-                <span className={`downarrow ${showSubmenu.programming ? 'open' : ''}`}>
-                  <i className="fa fa-caret-down"></i>
-                </span>
-              </a>
-              <ul className={`sub-menu-dropdown ${showSubmenu.programming ? 'open' : ''}`}>
-                <li>
-                  <a href="">Web Development</a>
-                </li>
-                <li>
-                  <a href="">Mobile App Development</a>
-                </li>
-                <li>
-                  <a href="">Data Science</a>
-                </li>
-                <li>
-                  <a href="">Game Development</a>
-                </li>
-              </ul>
-            </li>
+          {/* Categories */}
+          <a
+            href="#"
+            className={`logotype ${showCategories ? 'show' : ''}`}
+            onClick={toggleCategories}
+          >
+            Categories
+            <span className={`downarrow ${showCategories ? 'open' : ''}`}>
+              <i className="fa fa-caret-down"></i>
+            </span>
+          </a>
 
-            {/* Technology submenu */}
-            <li>
-              <a href="#" onClick={() => toggleSubmenu('technology')}>
-                Technology
-                <span className={`downarrow ${showSubmenu.technology ? 'open' : ''}`}>
-                  <i className="fa fa-caret-down"></i>
-                </span>
-              </a>
-              <ul className={`sub-menu-dropdown ${showSubmenu.technology ? 'open' : ''}`}>
-                <li>
-                  <a href="">Cybersecurity</a>
-                </li>
-                <li>
-                  <a href="">Cloud Computing</a>
-                </li>
-                <li>
-                  <a href="">Internet of Things (IoT)</a>
-                </li>
-                <li>
-                  <a href="">Artificial Intelligence (AI)</a>
-                </li>
-              </ul>
-            </li>
+          {/* Programming submenu */}
+          {showCategories && (
+            <ul className={`menu-dropdown ${showCategories ? 'open' : ''}`}>
+              <li>
+                <a href="#" onClick={() => toggleSubmenu('programming')}>
+                  Programming
+                  <span className={`downarrow ${showSubmenu.programming ? 'open' : ''}`}>
+                    <i className="fa fa-caret-down"></i>
+                  </span>
+                </a>
+                <ul className={`sub-menu-dropdown ${showSubmenu.programming ? 'open' : ''}`}>
+                  <li>
+                    <a href="">Web Development</a>
+                  </li>
+                  <li>
+                    <a href="">Mobile App Development</a>
+                  </li>
+                  <li>
+                    <a href="">Data Science</a>
+                  </li>
+                  <li>
+                    <a href="">Game Development</a>
+                  </li>
+                </ul>
+              </li>
 
-            {/* Engineering submenu */}
-            <li>
-              <a href="#" onClick={() => toggleSubmenu('engineering')}>
-                Engineering
-                <span className={`downarrow ${showSubmenu.engineering ? 'open' : ''}`}>
-                  <i className="fa fa-caret-down"></i>
-                </span>
-              </a>
-              <ul className={`sub-menu-dropdown ${showSubmenu.engineering ? 'open' : ''}`}>
-                <li>
-                  <a href="">Electrical</a>
-                </li>
-                <li>
-                  <a href="">Mechanical</a>
-                </li>
-                <li>
-                  <a href="">Civil</a>
-                </li>
-                <li>
-                  <a href="">Chemical</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        )}
-      </nav>
+              {/* Technology submenu */}
+              <li>
+                <a href="#" onClick={() => toggleSubmenu('technology')}>
+                  Technology
+                  <span className={`downarrow ${showSubmenu.technology ? 'open' : ''}`}>
+                    <i className="fa fa-caret-down"></i>
+                  </span>
+                </a>
+                <ul className={`sub-menu-dropdown ${showSubmenu.technology ? 'open' : ''}`}>
+                  <li>
+                    <a href="">Cybersecurity</a>
+                  </li>
+                  <li>
+                    <a href="">Cloud Computing</a>
+                  </li>
+                  <li>
+                    <a href="">Internet of Things (IoT)</a>
+                  </li>
+                  <li>
+                    <a href="">Artificial Intelligence (AI)</a>
+                  </li>
+                </ul>
+              </li>
+
+              {/* Engineering submenu */}
+              <li>
+                <a href="#" onClick={() => toggleSubmenu('engineering')}>
+                  Engineering
+                  <span className={`downarrow ${showSubmenu.engineering ? 'open' : ''}`}>
+                    <i className="fa fa-caret-down"></i>
+                  </span>
+                </a>
+                <ul className={`sub-menu-dropdown ${showSubmenu.engineering ? 'open' : ''}`}>
+                  <li>
+                    <a href="">Electrical</a>
+                  </li>
+                  <li>
+                    <a href="">Mechanical</a>
+                  </li>
+                  <li>
+                    <a href="">Civil</a>
+                  </li>
+                  <li>
+                    <a href="">Chemical</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          )}
+        </nav>
+      )}
     </div>
   );
 }
