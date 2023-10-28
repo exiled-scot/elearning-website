2e8dd13b7f80684943846e68f0901067a4ad282f
armitage <scott@nihilanth.co.uk>
2023-10-28 17:16:47 +0100

    educative sidebar and footer rewrite

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,21 +6,21 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-column">
-        <Link to="/business" className="footer-column-title">Business</Link>
+        <h2 className="footer-column-title">Business</h2>
         <a href="/#" className="footer-link">Teach</a>
         <a href="/#" className="footer-link">Get the app</a>
-        <Link to="/about" className="footer-link">About us</Link> {/* Update the link for "About us" */}
+        <Link to="/about" className="footer-link">About us</Link>
         <a href="/#" className="footer-link">Contact us</a>
       </div>
       <div className="footer-column">
-        <Link to="/careers" className="footer-column-title">Careers</Link>
+        <h2 className="footer-column-title">Careers</h2>
         <a href="/#" className="footer-link">Blog</a>
         <a href="/#" className="footer-link">Help and Support</a>
         <a href="/#" className="footer-link">Affiliate</a>
         <a href="/#" className="footer-link">Investors</a>
       </div>
       <div className="footer-column">
-        <Link to="/legal" className="footer-column-title">Legal</Link>
+        <h2 className="footer-column-title">Legal</h2>
         <a href="/#" className="footer-link">Terms</a>
         <a href="/#" className="footer-link">Privacy policy</a>
         <a href="/#" className="footer-link">Sitemap</a>
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,225 +1,57 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './NavMenu.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FiBook, FiSearch, FiCloud, FiUser, FiActivity, FiLayers, FiClipboard } from "react-icons/fi";
+import "./NavMenu.css";
 
-function NavMenu() {
-  const [showNav, setShowNav] = useState(false);
-  const [showCategories, setShowCategories] = useState(false);
-  const [showSubmenu, setShowSubmenu] = useState({
-    programming: false,
-    technology: false,
-    engineering: false
-  });
-
-  const primaryNavRef = useRef(null);
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (!primaryNavRef.current) return;
-
-      if (event.keyCode === 27) {
-        // ESC key: Close the menu
-        resetMenu();
-      } else if (event.keyCode === 37) {
-        // Left arrow key: Close the submenu
-        setShowSubmenu((prev) => ({
-          ...prev,
-          programming: false,
-          technology: false,
-          engineering: false
-        }));
-      } else if (event.keyCode === 39) {
-        // Right arrow key: Open the submenu of the selected category
-        setShowSubmenu((prev) => ({
-          ...prev,
-          [showCategories]: true
-        }));
-      } else if (event.keyCode === 40) {
-        // Down arrow key: Navigate to next item in the submenu
-        event.preventDefault();
-        const currentElement = document.activeElement;
-        if (currentElement.nextElementSibling) {
-          currentElement.nextElementSibling.focus();
-        } else {
-          currentElement.parentElement.firstElementChild.focus();
-        }
-      } else if (event.keyCode === 38) {
-        // Up arrow key: Navigate to previous item in the submenu
-        event.preventDefault();
-        const currentElement = document.activeElement;
-        if (currentElement.previousElementSibling) {
-          currentElement.previousElementSibling.focus();
-        } else {
-          currentElement.parentElement.lastElementChild.focus();
-        }
-      }
-    };
-
-    document.addEventListener('keydown', handleKeyDown);
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [showCategories]);
-
-  const toggleNav = () => {
-    setShowNav(!showNav);
-  };
-
-  const toggleCategories = () => {
-    setShowCategories(!showCategories);
-    setShowSubmenu({
-      programming: false,
-      technology: false,
-      engineering: false
-    });
-  };
-
-  const toggleSubmenu = (submenu) => {
-    setShowSubmenu((prev) => ({
-      ...prev,
-      [submenu]: !prev[submenu]
-    }));
-  };
-
-  const resetMenu = () => {
-    setShowNav(false);
-    setShowCategories(false);
-    setShowSubmenu({
-      programming: false,
-      technology: false,
-      engineering: false
-    });
-  };
-
+const NavMenu = () => {
   return (
-    <div
-      className={`primary-nav ${showNav ? 'show' : ''}`}
-      onMouseEnter={toggleNav}
-      onMouseLeave={resetMenu}
-    >
-      {showNav && (
-        <nav role="navigation" className="menu">
-          {/* Categories */}
-          <a
-            href="#"
-            className={`logotype ${showCategories ? 'show' : ''}`}
-            onClick={toggleCategories}
-            tabIndex="0"
-            ref={primaryNavRef}
-          >
-            Categories
-            <span className={`downarrow ${showCategories ? 'open' : ''}`}>
-              <i className="fa fa-caret-down"></i>
-            </span>
-          </a>
-
-          {/* Dashboard */}
-          <li aria-hidden={!showCategories}>
-            <a href="/dashboard">Dashboard</a>
-            <span className="icon">
-              <i className="fa fa-dashboard"></i>
-            </span>
-          </li>
-
-          {/* Programming submenu */}
-          {showCategories && (
-            <ul className={`menu-dropdown ${showCategories ? 'open' : ''}`}>
-              <li>
-                <a
-                  href="/category/programming"
-                  onClick={() => toggleSubmenu('programming')}
-                  tabIndex={showSubmenu.programming ? '0' : '-1'}
-                >
-                  Programming
-                  <span className={`downarrow ${showSubmenu.programming ? 'open' : ''}`}>
-                    <i className="fa fa-caret-down"></i>
-                  </span>
-                </a>
-                <ul
-                  className={`sub-menu-dropdown ${showSubmenu.programming ? 'open' : ''}`}
-                  tabIndex={showSubmenu.programming ? '0' : '-1'}
-                >
-                  <li>
-                    <a href="/category/programming/web-development">Web Development</a>
-                  </li>
-                  <li>
-                    <a href="/category/programming/mobile-app-development">Mobile App Development</a>
-                  </li>
-                  <li>
-                    <a href="/category/programming/data-science">Data Science</a>
-                  </li>
-                  <li>
-                    <a href="/category/programming/game-development">Game Development</a>
-                  </li>
-                </ul>
-              </li>
-
-              {/* Technology submenu */}
-              <li>
-                <a
-                  href="/category/technology"
-                  onClick={() => toggleSubmenu('technology')}
-                  tabIndex={showSubmenu.technology ? '0' : '-1'}
-                >
-                  Technology
-                  <span className={`downarrow ${showSubmenu.technology ? 'open' : ''}`}>
-                    <i className="fa fa-caret-down"></i>
-                  </span>
-                </a>
-                <ul
-                  className={`sub-menu-dropdown ${showSubmenu.technology ? 'open' : ''}`}
-                  tabIndex={showSubmenu.technology ? '0' : '-1'}
-                >
-                  <li>
-                    <a href="/category/technology/cybersecurity">Cybersecurity</a>
-                  </li>
-                  <li>
-                    <a href="/category/technology/cloud-computing">Cloud Computing</a>
-                  </li>
-                  <li>
-                    <a href="/category/technology/iot">Internet of Things (IoT)</a>
-                  </li>
-                  <li>
-                    <a href="/category/technology/ai">Artificial Intelligence (AI)</a>
-                  </li>
-                </ul>
-              </li>
-
-              {/* Engineering submenu */}
-              <li>
-                <a
-                  href="/category/engineering"
-                  onClick={() => toggleSubmenu('engineering')}
-                  tabIndex={showSubmenu.engineering ? '0' : '-1'}
-                >
-                  Engineering
-                  <span className={`downarrow ${showSubmenu.engineering ? 'open' : ''}`}>
-                    <i className="fa fa-caret-down"></i>
-                  </span>
-                </a>
-                <ul
-                  className={`sub-menu-dropdown ${showSubmenu.engineering ? 'open' : ''}`}
-                  tabIndex={showSubmenu.engineering ? '0' : '-1'}
-                >
-                  <li>
-                    <a href="/category/engineering/electrical">Electrical</a>
-                  </li>
-                  <li>
-                    <a href="/category/engineering/mechanical">Mechanical</a>
-                  </li>
-                  <li>
-                    <a href="/category/engineering/civil">Civil</a>
-                  </li>
-                  <li>
-                    <a href="/category/engineering/chemical">Chemical</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          )}
-        </nav>
-      )}
-    </div>
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            <FiBook className="nav-icon" />
+            <span className="nav-text">My Learning</span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            <FiSearch className="nav-icon" />
+            <span className="nav-text">Explore</span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            <FiCloud className="nav-icon" />
+            <span className="nav-text">Cloud Labs</span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            <FiUser className="nav-icon" />
+            <span className="nav-text">Personalised Paths</span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            <FiActivity className="nav-icon" />
+            <span className="nav-text">Projects</span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            <FiLayers className="nav-icon" />
+            <span className="nav-text">Skill Paths</span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" activeClassName="active">
+            <FiClipboard className="nav-icon" />
+            <span className="nav-text">Assessments</span>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
-}
+};
 
 export default NavMenu;
