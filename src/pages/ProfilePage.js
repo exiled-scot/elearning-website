f8758219af2c19bd093bb4689541fb6e9c7fac26
armitage <scott@nihilanth.co.uk>
2023-12-08 19:29:47 +0000

    profile page authentication

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { getUserId } from "../utils/auth";
 
 const ProfilePage = ({ user }) => {
-  return (
-    <div>
-      <h1>Profile Page:</h1>
-      <h1>{user.id}</h1>
-      <h2>{user.name}</h2>
-      <h2>{user.email}</h2>
-    </div>
-  );
+  if (user.id === getUserId()) {
+    return (
+      <div>
+        <h1>Profile Page:</h1>
+        <h1>{user.id}</h1>
+        <h2>{user.name}</h2>
+        <h2>{user.email}</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Unauthorized</h1>
+      </div>
+    );
+  }
 };
 
 export default ProfilePage;
