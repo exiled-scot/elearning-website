fc07c049172c594683453c7d1955bde02fd31af8
Scott Stevenson <scott@nihilanth.co.uk>
2024-04-28 16:54:09 +0100

    typo

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -11,44 +11,28 @@ const CourseTitle = ({ title }) => {
 
 const CourseImage = ({ id, image }) => {
   const imageUrl = `http://localhost:5002/api/files/zahet9k8sqf34u9/${id}/${image}?token=`;
-
   return <img src={imageUrl} alt="image" className="card-image" />;
 };
 
 const CourseContent = ({ content }) => {
-  if (content === null) {
+  if (!content || !content.course_content || content.course_content.length === 0) {
     return null;
   }
 
-  let parsedContent;
-
-  try {
-    parsedContent = content;
-  } catch (error) {
-    console.error('Error parsing content:', error);
-    parsedContent = {};
-  }
-  const courseContentList = parsedContent.course_content?.map((item, index) => (
+  const courseContentList = content.course_content.map((item, index) => (
     <li key={index}>{item}</li>
   ));
 
-  if (parsedContent?.course_content?.length > 0) {
-    return (
-      parsedContent.course_content && (
-        <div className="two-column-list">
-          <h2>What You'll Learn:</h2>
-          <ul>{courseContentList}</ul>
-        </div>
-      )
-    );
-  }
-
-  return null;
+  return (
+    <div className="two-column-list">
+      <h2>What You'll Learn:</h2>
+      <ul>{courseContentList}</ul>
+    </div>
+  );
 };
 
 const BuyCourse = ({ course }) => {
   const { title, id, instructor, image, content, description, requirements, reviews } = course;
-
   return (
     <div className="buy-course--sidebar">
       <CourseImage id={id} image={image} />
@@ -73,16 +57,19 @@ const BuyCourse = ({ course }) => {
 
 const ProductPage = ({ course }) => {
   const { title, id, instructor, image, content, description, requirements, reviews } = course;
-
+  console.log(course);
   return (
     <div>
       <CourseImage id={id} image={image} />
       <CourseTitle title={title} />
       <div>
-        <h2>{description.match(/^.+?[.!?](\s|$)/)[0]}</h2>
+        <h2>{description?.match(/^.+?\[.!?\](\\s|$)/)[0] || ''}</h2>
       </div>
       <div>
-        Created by <a href={`/instructors/${slugify(instructor)}`} style={{ textDecoration: 'underline', color: 'blue' }}>{instructor}</a>
+        Created by {' '}
+        <a href={`/instructors/${slugify(instructor)}`} style={{ textDecoration: 'underline', color: 'blue' }}>
+          {instructor}
+        </a>
       </div>
       <CourseContent content={content} className="course-requirements" />
       <ReadMore>{description}</ReadMore>
