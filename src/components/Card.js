f5f657dae945f1674ce8e5402afcfc4f771b97cb
Scott Stevenson <scott@nihilanth.co.uk>
2024-04-28 16:15:30 +0100

    fix error when undefined

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Card.css";
 import slugify from "slugify";
 
-const Card = ({ courses }) => {
+const Card = ({ courses = [] }) => {
   const [hoveredCourseId, setHoveredCourseId] = useState(null);
 
   const handleMouseEnter = (courseId) => {
@@ -16,49 +16,54 @@ const Card = ({ courses }) => {
 
   return (
     <div className="card-container">
-      {courses.map((course) => (
-        <Link
-          to={`/courses/${slugify(course.title)}`}
-          className="card-link"
-          style={{ textDecoration: "none", color: "inherit" }}
-        >
-          <div
-            className={`card ${hoveredCourseId === course.id ? "hovered" : ""}`}
+      {courses &&
+        courses.map((course) => (
+          <Link
+            to={`/courses/${slugify(course.title)}`}
+            className="card-link"
+            style={{ textDecoration: "none", color: "inherit" }}
             key={course.id}
-            onMouseEnter={() => handleMouseEnter(course.id)}
-            onMouseLeave={handleMouseLeave}
           >
-            <div className="image-wrapper">
-              <img
-                src={`http://localhost:5002/api/files/zahet9k8sqf34u9/${course.id}/${course.image}?token=`}
-                alt="image"
-                className="card-image"
-              />
-              <div className="image-overlay"></div>
-            </div>
-            <div className="wrapper">
-              <div className="content">
-                <h2 className="title">{course.title}</h2>
-                <p className="instructor">{course.instructor}</p>
-                <div className="description-container">
-                  <p className="description">{course.description}</p>
-                  <div className="description-fade"></div>
-                </div>
+            <div
+              className={`card ${
+                hoveredCourseId === course.id ? "hovered" : ""
+              }`}
+              onMouseEnter={() => handleMouseEnter(course.id)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <div className="image-wrapper">
+                <img
+                  src={`http://localhost:5002/api/files/zahet9k8sqf34u9/${course.id}/${course.image}?token=`}
+                  alt="image"
+                  className="card-image"
+                />
+                <div className="image-overlay"></div>
               </div>
-              <button
-                 className="button-28 buy-button"
-                 onClick={(event) => {
-                   event.preventDefault();
-                   event.stopPropagation();
-                   window.location.href = `/courses/${slugify(course.title)}/purchase`;
-                }}
-              >
-                Buy this course
-              </button>
+              <div className="wrapper">
+                <div className="content">
+                  <h2 className="title">{course.title}</h2>
+                  <p className="instructor">{course.instructor}</p>
+                  <div className="description-container">
+                    <p className="description">{course.description}</p>
+                    <div className="description-fade"></div>
+                  </div>
+                </div>
+                <button
+                  className="button-28 buy-button"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    window.location.href = `/courses/${slugify(
+                      course.title
+                    )}/purchase`;
+                  }}
+                >
+                  Buy this course
+                </button>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
     </div>
   );
 };
